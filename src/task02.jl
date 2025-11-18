e27b63081f586244b89259bf69e47142a11c58e9
polbuta <pol.buta@gmail.com>
2025-11-18 20:54:43 +0300

    \task02.jl

--- src/task02.jl
+++ src/task02.jl
@@ -12,7 +12,7 @@ end
 function mul35sum(N::Integer)::Int64
     sum = Int64(0);
     for i in 1:N-1
-        if i%3==0 && i%5==0
+        if i%3==0 || i%5==0
             sum+=i;
         end
     end
